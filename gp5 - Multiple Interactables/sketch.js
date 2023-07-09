06a79899407d2243993490d1b45adaa8a209f109
Jacques Thurling <j.thurling@capital-sa.com>
2023-07-09 13:18:48 +0100

    [Update]: Starting GP 5

--- gp5 - Multiple Interactables/sketch.js
+++ gp5 - Multiple Interactables/sketch.js
@@ -91,51 +91,6 @@ function draw()
 
 	///////////LOGIC CODE//////////
 
-	// Check if trees are on screen and alter the array accordingly
-	for (let index = 0; index < trees_x.length; index++) {
-		if ((trees_x[index] > canyon.x_pos && trees_x[index] < canyon.x_pos + canyon.width)
-		|| (trees_x[index] + 70 > canyon.x_pos && trees_x[index] + 70 < canyon.x_pos + canyon.width)) {
-			trees_x.splice(index, 1);
-			continue;
-		}
-		if (trees_x[index] < -100) {
-			trees_x.splice(0, 1);
-			trees_x.push(width + 50);
-		} 
-		else if (trees_x[index] > width + 100) {
-			trees_x.pop();
-			trees_x.unshift(-50);
-		}
-	}
-
-	// Check if clouds are on screen and alter the array accordingly
-	for (let index = 0; index < clouds.length; index++) {
-		if (clouds[index].x_pos < -100) {
-			clouds.splice(0, 1);
-			clouds.push({
-				x_pos: width + 50, 
-				y_pos: random(0, 100),
-				size: random(50, 100)
-			});
-		} 
-		else if (clouds[index].x_pos > width + 100) {
-			clouds.pop();
-			clouds.unshift({
-				x_pos: -50, 
-				y_pos: random(0, 100),
-				size: random(50, 100)
-			});
-		}
-	}
-
-	
-	// Check if mountain is on screen
-	if (mountain.x_pos < -500) {
-		mountain.x_pos = width + 10;
-	} else if (mountain.x_pos > width + 550) {
-		mountain.x_pos = -480;
-	}
-
 	// Check if canyon is on screen
 	if (canyon.x_pos < -150) {
 		canyon.x_pos = width + 10;
@@ -149,65 +104,24 @@ function draw()
 
 	background(100,155,255); //fill the sky blue
 
-	// Draw the mountains.
-	fill(200);
-	triangle(mountain.x_pos, mountain.y_pos, mountain.x_pos + 320, mountain.y_pos, mountain.x_pos + 170, mountain.y_pos - 332);
-	fill(180);
-	triangle(mountain.x_pos + 150, mountain.y_pos, mountain.x_pos + 350, mountain.y_pos, mountain.x_pos + 250, mountain.y_pos - 232);
-	fill(170);
-	triangle(mountain.x_pos + 50, mountain.y_pos, mountain.x_pos + 250, mountain.y_pos, mountain.x_pos + 150, mountain.y_pos - 172);
+	// Check if mountain is on screen
+	drawMountains();
 
-	// add snow to mountain tops
-	fill(255);
-	triangle(mountain.x_pos + 120, mountain.y_pos - 122, mountain.x_pos + 150, mountain.y_pos - 172, mountain.x_pos + 192, mountain.y_pos - 102);
-	triangle(mountain.x_pos + 215, mountain.y_pos - 152, mountain.x_pos + 250, mountain.y_pos - 232, mountain.x_pos + 275, mountain.y_pos - 172);
-	triangle(mountain.x_pos + 144, mountain.y_pos - 280, mountain.x_pos + 206, mountain.y_pos - 250, mountain.x_pos + 170, mountain.y_pos - 332);
+	// Check if clouds are on screen and alter the array accordingly
+	drawClouds();
 
-	// Draw the clouds.
-	for (let index = 0; index < clouds.length; index++) {
-		fill(255, 255, 255);
-		ellipse(clouds[index].x_pos, clouds[index].y_pos, 80, 80);
-		ellipse(clouds[index].x_pos - 30, clouds[index].y_pos + 10, 60, 60);
-		ellipse(clouds[index].x_pos + 20, clouds[index].y_pos + 5, 70, 70);
-		ellipse(clouds[index].x_pos - 50, clouds[index].y_pos + 20, 40, 40);
-		ellipse(clouds[index].x_pos + 55, clouds[index].y_pos + 5, 70, 70);
-		ellipse(clouds[index].x_pos + 85, clouds[index].y_pos + 10, 50, 50);
-		ellipse(clouds[index].x_pos + 65, clouds[index].y_pos + 10, 60, 60);	
-	}
+	// Check if trees are on screen and alter the array accordingly
+	drawTrees();
 
 	noStroke();
 	fill(0,155,0);
 	rect(0, floorPos_y, width, height - floorPos_y); //draw some green ground
-
-	// Draw the Trees
-	for (let index = 0; index < trees_x.length; index++) {
-		fill(139, 69, 19);
-		rect(trees_x[index], treePos_y + 45, 50, 100);
-		fill(0, 145, 0);
-		triangle(trees_x[index] - 30, treePos_y + 50, trees_x[index] + 80, treePos_y + 50, trees_x[index] + 25, treePos_y - 50);
-		fill(0, 150, 0);
-		triangle(trees_x[index] - 30, treePos_y + 10, trees_x[index] + 80, treePos_y + 10, trees_x[index] + 25, treePos_y - 80);
-		fill(0, 155, 0);
-		triangle(trees_x[index] - 30, treePos_y - 30, trees_x[index] + 80, treePos_y - 30, trees_x[index] + 25, treePos_y - 110);
-	}
-
 	
-
 	// Draw the collectable items
-	if (!collectable.collected) {
-		fill(255, 215, 0);
-		ellipse(collectable.x_pos, collectable.y_pos, collectable.size, collectable.size);
-		fill(255, 255, 0);
-		ellipse(collectable.x_pos, collectable.y_pos, collectable.size - 10, collectable.size - 10);
-		fill(255, 215, 0);
-		ellipse(collectable.x_pos, collectable.y_pos, collectable.size - 20, collectable.size - 20);
-		fill(255, 255, 0);
-		ellipse(collectable.x_pos, collectable.y_pos, collectable.size - 30, collectable.size - 30);
-	}
+	drawCollectable();
 
-
-	// Draw a canyon
-	fill(100, 155, 255);
+	// Draw a canyon - brown rectangle
+	fill(139,69,19);
 	//blue rectangle going to the botton of the screen
 	rect(canyon.x_pos, 432, canyon.width, 144);
 
@@ -400,5 +314,102 @@ function keyReleased()
 		isRight = false;
 		console.log("Right Key Released");
 		console.log("isRight: " + isRight);
+	}
+}
+
+function drawMountains() {
+	if (mountain.x_pos < -500) {
+		mountain.x_pos = width + 10;
+	} else if (mountain.x_pos > width + 550) {
+		mountain.x_pos = -480;
+	}
+
+	// Draw the mountains.
+	fill(200);
+	triangle(mountain.x_pos, mountain.y_pos, mountain.x_pos + 320, mountain.y_pos, mountain.x_pos + 170, mountain.y_pos - 332);
+	fill(180);
+	triangle(mountain.x_pos + 150, mountain.y_pos, mountain.x_pos + 350, mountain.y_pos, mountain.x_pos + 250, mountain.y_pos - 232);
+	fill(170);
+	triangle(mountain.x_pos + 50, mountain.y_pos, mountain.x_pos + 250, mountain.y_pos, mountain.x_pos + 150, mountain.y_pos - 172);
+
+	// add snow to mountain tops
+	fill(255);
+	triangle(mountain.x_pos + 120, mountain.y_pos - 122, mountain.x_pos + 150, mountain.y_pos - 172, mountain.x_pos + 192, mountain.y_pos - 102);
+	triangle(mountain.x_pos + 215, mountain.y_pos - 152, mountain.x_pos + 250, mountain.y_pos - 232, mountain.x_pos + 275, mountain.y_pos - 172);
+	triangle(mountain.x_pos + 144, mountain.y_pos - 280, mountain.x_pos + 206, mountain.y_pos - 250, mountain.x_pos + 170, mountain.y_pos - 332);
+}
+
+function drawTrees() {
+	for (let index = 0; index < trees_x.length; index++) {
+		if ((trees_x[index] > canyon.x_pos && trees_x[index] < canyon.x_pos + canyon.width)
+		|| (trees_x[index] + 70 > canyon.x_pos && trees_x[index] + 70 < canyon.x_pos + canyon.width)) {
+			trees_x.splice(index, 1);
+			continue;
+		}
+		if (trees_x[index] < -100) {
+			trees_x.splice(0, 1);
+			trees_x.push(width + 50);
+		} 
+		else if (trees_x[index] > width + 100) {
+			trees_x.pop();
+			trees_x.unshift(-50);
+		}
+	}
+
+	for (let index = 0; index < trees_x.length; index++) {
+		fill(139, 69, 19);
+		rect(trees_x[index], treePos_y + 45, 50, 100);
+		fill(0, 145, 0);
+		triangle(trees_x[index] - 30, treePos_y + 50, trees_x[index] + 80, treePos_y + 50, trees_x[index] + 25, treePos_y - 50);
+		fill(0, 150, 0);
+		triangle(trees_x[index] - 30, treePos_y + 10, trees_x[index] + 80, treePos_y + 10, trees_x[index] + 25, treePos_y - 80);
+		fill(0, 155, 0);
+		triangle(trees_x[index] - 30, treePos_y - 30, trees_x[index] + 80, treePos_y - 30, trees_x[index] + 25, treePos_y - 110);
+	}
+}
+
+function drawClouds() {
+	for (let index = 0; index < clouds.length; index++) {
+		if (clouds[index].x_pos < -100) {
+			clouds.splice(0, 1);
+			clouds.push({
+				x_pos: width + 50, 
+				y_pos: random(0, 100),
+				size: random(50, 100)
+			});
+		} 
+		else if (clouds[index].x_pos > width + 100) {
+			clouds.pop();
+			clouds.unshift({
+				x_pos: -50, 
+				y_pos: random(0, 100),
+				size: random(50, 100)
+			});
+		}
+	}
+
+	// Draw the clouds.
+	for (let index = 0; index < clouds.length; index++) {
+		fill(255, 255, 255);
+		ellipse(clouds[index].x_pos, clouds[index].y_pos, 80, 80);
+		ellipse(clouds[index].x_pos - 30, clouds[index].y_pos + 10, 60, 60);
+		ellipse(clouds[index].x_pos + 20, clouds[index].y_pos + 5, 70, 70);
+		ellipse(clouds[index].x_pos - 50, clouds[index].y_pos + 20, 40, 40);
+		ellipse(clouds[index].x_pos + 55, clouds[index].y_pos + 5, 70, 70);
+		ellipse(clouds[index].x_pos + 85, clouds[index].y_pos + 10, 50, 50);
+		ellipse(clouds[index].x_pos + 65, clouds[index].y_pos + 10, 60, 60);	
+	}
+}
+
+function drawCollectable(){
+	if (!collectable.collected) {
+		fill(255, 215, 0);
+		ellipse(collectable.x_pos, collectable.y_pos, collectable.size, collectable.size);
+		fill(255, 255, 0);
+		ellipse(collectable.x_pos, collectable.y_pos, collectable.size - 10, collectable.size - 10);
+		fill(255, 215, 0);
+		ellipse(collectable.x_pos, collectable.y_pos, collectable.size - 20, collectable.size - 20);
+		fill(255, 255, 0);
+		ellipse(collectable.x_pos, collectable.y_pos, collectable.size - 30, collectable.size - 30);
 	}
 }
